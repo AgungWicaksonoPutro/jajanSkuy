543494d4be9e8fcb1294953fcefbeecdce18fbdd
AgungWicaksonoPutro <agungwicaksonoputro.dev@gmail.com>
2021-01-02 00:29:44 +0700

    Developing

--- liffSetup.js
+++ liffSetup.js
@@ -1,6 +1,28 @@
 window.onload = () => {
-    const LiffId = "1655536452-40OE6Vzz"
-    initializeLiffOrDie(LiffId)
+    const useNodeJS = false;   // if you are not using a node server, set this value to false
+    const defaultLiffId = "1655536452-40OE6Vzz";   // change the default LIFF value if you are not using a node server
+
+    // DO NOT CHANGE THIS
+    let myLiffId = "";
+
+    // if node is used, fetch the environment variable and pass it to the LIFF method
+    // otherwise, pass defaultLiffId
+    if (useNodeJS) {
+        fetch('/send-id')
+            .then(function (reqResponse) {
+                return reqResponse.json();
+            })
+            .then(function (jsonResponse) {
+                myLiffId = jsonResponse.id;
+                initializeLiffOrDie(myLiffId);
+            })
+            .catch((err)=> {
+                console.log(err)
+            });
+    } else {
+        myLiffId = defaultLiffId;
+        initializeLiffOrDie(myLiffId);
+    }
 }
 
 function initializeLiffOrDie(LiffId){
